ff8210c2b76fa719d46f2fc7aaf56081cc3d9d3a
Nicolas Le Goff <legoff.n@gmail.com>
2012-11-09 18:24:37 +0100

    Update template

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 var entries = Joshfire.factory.getDataSource("main");
 entries.find(
   {
@@ -5,20 +7,23 @@ entries.find(
   },
   function (err, data) {
     var html = "";
-    _.map(data.entries, function (entry, idx) {
-      html += "&lt;li&gt;&lt;span class='title'>" + entry.name + "&lt;/span>"
-        + "&lt;br/&gt;" + entry.description
-        + "&lt;br/&gt;" + entry.datePublished;
-      if (entry.image) {
-        html += "&lt;span class='thumbnail'&gt;&lt;img src='"
-          + entry.image.contentURL
-          + "' alt='" + entry.image.name + "' /&gt;"
-          + "&lt;/span&gt;";
-      }
+    _.map(data.entries, function (feed, idx) {
+         html += "<span class='title'>" + feed.name + "</span><div>"
+        _.map(feed.entries, function (entry, idx) {
+            html += "<span class='title'>" + entry.name + "</span>"
+            + "<br/>" + entry.description
+
+            if (entry.thumbnail) {
+                _.map(entry.thumbnail, function (thumb, idx) {
+                    html += "<span class='thumbnail'>" + thumb.name + " <br /><img src='"
+                    + thumb.contentURL
+                    + "' alt='" + thumb.name + "' />"
+                    + "</span><br />";
+                });
+            }
+        });
+        html += "</div>";
     });
     document.getElementById('entries').innerHTML = html;
   }
 );
-
-
-
